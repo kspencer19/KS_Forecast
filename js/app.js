8194f39b4bea470af98fb3d6783dde21a0fed058
Kolten <kolten.spencer808@gmail.com>
2022-11-17 08:57:27 -0700

    2nd commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-$.ajax("http://magicseaweed.com/api/AIzaSyArqgiF3Z6SNkUbvKb_BExIo3idchv19W0/forecast/?spot_id=3085/")
-// $.ajax("https://api.icndb.com/jokes/random")
-.then((data) => {
+// $.ajax("https://magicseaweed.com/api/AIzaSyArqgiF3Z6SNkUbvKb_BExIo3idchv19W0/forecast/?spot_id=3085/")
+// // $.ajax("https://api.icndb.com/jokes/random")
+// .then((data) => {
      console.log("data")
-})
+// })
