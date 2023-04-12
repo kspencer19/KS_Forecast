0b3bb581793b8ab421bb2bc4aa51fcf0a0dd7187
Kolten <54643900+kspencer19@users.noreply.github.com>
2023-04-11 22:19:01 -1000

    Add files via upload

--- js/app.js
+++
@@ -1,5 +0,0 @@
-$.ajax("https://magicseaweed.com/api/AIzaSyArqgiF3Z6SNkUbvKb_BExIo3idchv19W0/forecast/?spot_id=3085/")
-// $.ajax("https://api.icndb.com/jokes/random")
-.then((data) => {
-     console.log("data")
-})
